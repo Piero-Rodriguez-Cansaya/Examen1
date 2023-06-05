4ce7f64cc022a848764772bf58948258aa381c5d
Piero-Rodriguez-Cansaya <Piero-Rodriguez-Cansaya@users.noreply.github.com>
2023-06-05 11:51:01 -0600

    first commit

---
+++ dsw1-S2/src/main/java/desarrolloweb/Semana2/dsw1S2/Dsw1S2Application.java
@@ -0,0 +1,13 @@
+package desarrolloweb.Semana2.dsw1S2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsw1S2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsw1S2Application.class, args);
+	}
+
+}
---
+++ dsw1-S2/src/test/java/desarrolloweb/Semana2/dsw1S2/Dsw1S2ApplicationTests.java
@@ -0,0 +1,13 @@
+package desarrolloweb.Semana2.dsw1S2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw1S2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
